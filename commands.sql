b4ad5ab03a23441dc72f7b5dbf7322cce03f734c
Roman <romchikus1987@gmail.com>
2024-03-07 16:05:28 +0200

    updated

--- commands.sql
+++ commands.sql
@@ -15,3 +15,7 @@ INSERT INTO blogs (author, url, title, likes) VALUES
     ('John Doe', 'http://example.com/blog1', 'First Blog', 10),
     ('Jane Smith', 'http://example.com/blog2', 'Second Blog', 5);
 ```
+- selecting rows:
+```sql
+  select * from blogs;
+```
