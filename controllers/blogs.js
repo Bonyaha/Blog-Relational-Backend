848327e1885e5e7c55f179986e4a7df20faf8ba4
Roman <romchikus1987@gmail.com>
2024-03-11 16:53:50 +0200

    usage of express-async-errors library

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,56 +1,34 @@
 const router = require('express').Router()
 const { Blog } = require('../models')
 
-router.get('/', async (req, res, next) => {
-	try {
-		const blogs = await Blog.findAll()
-		console.log(JSON.stringify(blogs, null, 2))
-		res.json(blogs)
-	} catch (error) {
-		next(error)
-	}
+router.get('/', async (req, res) => {
+	const blogs = await Blog.findAll()
+	console.log(JSON.stringify(blogs, null, 2))
+	res.json(blogs)
 })
 
-router.get('/:id', blogFinder, async (req, res, next) => {
-	try {
-		res.json(req.blog) //we get req.blog from blogFinder middleware
-	} catch (error) {
-		next(error)
-	}
+router.get('/:id', blogFinder, async (req, res) => {
+	res.json(req.blog) //we get req.blog from blogFinder middleware
 })
 
 
-router.post('/', async (req, res, next) => {
-	try {
-		const blog = await Blog.create(req.body)
-		return res.status(201).json(blog)
-	} catch (error) {
-		next(error)
-	}
+router.post('/', async (req, res) => {
+	const blog = await Blog.create(req.body)
+	return res.status(201).json(blog)
 })
 
 
-router.delete('/:id', blogFinder, async (req, res, next) => {
-	try {
-		await req.blog.destroy()
-		return res.status(204).end()
-	} catch (error) {
-		next(error)
-	}
+router.delete('/:id', blogFinder, async (req, res) => {
+	await req.blog.destroy()
+	return res.status(204).end()
 })
 
-router.put('/:id', blogFinder, async (req, res, next) => {
-	try {
-		req.blog.likes = req.body.likes
-		await req.blog.save()
+router.put('/:id', blogFinder, async (req, res) => {
+	req.blog.likes = req.body.likes
+	await req.blog.save()
 
-		res.json(req.blog)
-	} catch (error) {
-		next(error)
-	}
+	res.json(req.blog)
+
 })
-
-
-
 
 module.exports = router
